022bc728a8ef2e336cb8d144d4753652dddc1b5c
tanyunjian <13142315147@163.com>
2019-08-28 09:41:14 +0800

    加入显示天气信息的功能。

--- app/src/main/java/com/miki/coolweather/util/Utility.java
+++ app/src/main/java/com/miki/coolweather/util/Utility.java
@@ -2,9 +2,11 @@ package com.miki.coolweather.util;
 
 import android.text.TextUtils;
 
+import com.google.gson.Gson;
 import com.miki.coolweather.db.City;
 import com.miki.coolweather.db.County;
 import com.miki.coolweather.db.Province;
+import com.miki.coolweather.gson.Weather;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -77,5 +79,19 @@ public class Utility {
             }
         }
         return false;
+    }
+    /**
+     * 将返回的JSON数据解析成Weather实体类
+     */
+    public static Weather handleWeatherResponse(String response){
+        try{
+            JSONObject jsonObject = new JSONObject(response);
+            JSONArray jsonArray = jsonObject.getJSONArray("HeWeather");
+            String weatherContent = jsonArray.getJSONObject(0).toString();
+            return new Gson().fromJson(weatherContent, Weather.class);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return null;
     }
 }
