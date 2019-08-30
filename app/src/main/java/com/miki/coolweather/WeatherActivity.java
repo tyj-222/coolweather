d27d8507a6d56fb1b3cb75b262e25c990f87962a
tanyunjian <13142315147@163.com>
2019-08-30 11:51:15 +0800

    完成

--- app/src/main/java/com/miki/coolweather/WeatherActivity.java
+++ app/src/main/java/com/miki/coolweather/WeatherActivity.java
@@ -44,6 +44,7 @@ public class WeatherActivity extends AppCompatActivity {
     private  String mWeatherId;
     //下拉刷新天气
     public SwipeRefreshLayout swipeRefresh;
+
     private ScrollView weatherLayout;
     private TextView titleCity;
     private TextView titleUpdateTime;
@@ -94,21 +95,17 @@ public class WeatherActivity extends AppCompatActivity {
         if(weatherString != null){
             //有缓存时直接解析天气数据
             Weather weather = Utility.handleWeatherResponse(weatherString);
-           // weatherId = weather.basic.weatherId;
             mWeatherId =weather.basic.weatherId;
             showWeatherInfo(weather);
         }else {
             //无缓存时去服务器查询天气
-           // weatherId = getIntent().getStringExtra("weather_id");
             mWeatherId = getIntent().getStringExtra("weather_id");
             weatherLayout.setVisibility(View.INVISIBLE);
-           // requestWeather(weatherId);
             requestWeather(mWeatherId);
         }
         swipeRefresh.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
             @Override
             public void onRefresh() {
-            //    requestWeather(weatherId);
                 requestWeather(mWeatherId);
             }
         });
